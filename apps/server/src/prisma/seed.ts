844da5092da605b33ce251869f3fc6822339b8cd
Everton Pereira <everton.mwp@gmail.com>
2023-02-21 22:51:57 +0200

    fix: seed script and add some timers

--- apps/server/src/prisma/seed.ts
+++ apps/server/src/prisma/seed.ts
@@ -4,46 +4,63 @@ import { join } from 'path';
 import prisma from './client';
 
 async function main() {
+  const startTime = Date.now();
+
+  console.log('🌱 DB Seeding started, may take some time...');
+
+  const interval = setInterval(() => {
+    const msElapsed = Date.now() - startTime;
+    console.log(`⏳ DB Seeding in progress... Elapsed: ${msElapsed}ms`);
+  }, 10000);
+
+  const msElapsed = Date.now() - startTime;
+
   // Seed categories
-  // await new Promise((resolve, reject) => {
-  //   createReadStream(join(process.cwd(), './src/prisma/data/categories.csv'))
-  //     .pipe(parse({ delimiter: ',', from_line: 2, relaxQuotes: true }))
-  //     .on('data', async function (row: string[]) {
-  //       const [id, name, color] = row.map((item) => item.replace(/"/g, ''));
-  //       return await prisma.categories.create({ data: { id, name, color } });
-  //     })
-  //     .on('end', () => {
-  //       resolve(true);
-  //     })
-  //     .on('error', (error) => {
-  //       reject(error);
-  //     });
-  // });
+  await new Promise((resolve, reject) => {
+    createReadStream(join(process.cwd(), './src/prisma/data/categories.csv'))
+      .pipe(parse({ delimiter: ',', from_line: 2, relaxQuotes: true }))
+      .on('data', async function (row: string[]) {
+        const [id = '', name = '', color = ''] = row?.map((item) =>
+          item?.replace(/"/g, ''),
+        );
+        if (!id || !name) return;
+        return await prisma.categories.create({ data: { id, name, color } });
+      })
+      .on('end', () => {
+        resolve(true);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
 
   // Seed accounts
-  // await new Promise((resolve, reject) => {
-  //   createReadStream(join(process.cwd(), './src/prisma/data/accounts.csv'))
-  //     .pipe(parse({ delimiter: ',', from_line: 2, quote: '' }))
-  //     .on('data', async function (row: string[]) {
-  //       const [id, name, bank] = row.map((item) => item.replace(/"/g, ''));
-  //       return await prisma.accounts.create({ data: { id, name, bank } });
-  //     })
-  //     .on('end', () => {
-  //       resolve(true);
-  //     })
-  //     .on('error', (error) => {
-  //       reject(error);
-  //     });
-  // });
+  await new Promise((resolve, reject) => {
+    createReadStream(join(process.cwd(), './src/prisma/data/accounts.csv'))
+      .pipe(parse({ delimiter: ',', from_line: 2, quote: '' }))
+      .on('data', async function (row: string[]) {
+        const [id = '', name = '', bank = ''] = row?.map((item) =>
+          item?.replace(/"/g, ''),
+        );
+        if (!id || !name) return;
+        return await prisma.accounts.create({ data: { id, name, bank } });
+      })
+      .on('end', () => {
+        resolve(true);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
 
   // Seed transactions
   await new Promise((resolve, reject) => {
     createReadStream(join(process.cwd(), './src/prisma/data/transactions.csv'))
       .pipe(parse({ delimiter: ',', from_line: 2, relaxQuotes: true }))
       .on('data', async function (row: string[]) {
-        if (row.length === 7) {
+        if (row?.length === 7) {
           const [id, accountId, categoryId, reference, amount, currency, date] =
-            row.map((item) => item.replace(/"/g, '').trim());
+            row?.map((item) => item?.replace(/"/g, '').trim());
           if (id && accountId && categoryId) {
             return await prisma.transactions.createMany({
               data: {
@@ -66,6 +83,10 @@ async function main() {
         reject(error);
       });
   });
+
+  clearInterval(interval);
+  console.log('🌱 DB Seeding endend!');
+  console.log(` ⏳ Seed completed in ${msElapsed}ms ⌛`);
 }
 
 main()
